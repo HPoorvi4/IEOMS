3856b2cf8bd875083b7e644a24db112a22d81f83
HPoorvi4 <hpoorvi2004@gmail.com>
2026-01-09 11:42:10 +0530

    Final edits

--- backend/controllers/geminiController.js
+++ backend/controllers/geminiController.js
@@ -1,6 +1,6 @@
 const axios = require('axios');
 
-const GEMINI_API_KEY = 'AIzaSyCr71f2h4yB9VeGRJKiedUH_S0DAbBBtUo';
+const GEMINI_API_KEY = process.env.GEMINI_API_KEY;
 const GEMINI_API_URL = 'https://generativelanguage.googleapis.com/v1beta/models/gemini-2.0-flash:generateContent';
 
 async function getGeminiRecommendations(energyData) {
